abbb071a2914466cbc8b69f61641714621b23d21
sina <info@sinaghadri.com>
2023-12-19 01:21:19 +0330

    fix - remove unnecessary props

--- src/components/Dropdown/DropdownItemSelected.tsx
+++ src/components/Dropdown/DropdownItemSelected.tsx
@@ -1,14 +1,9 @@
 import { memo, FC } from 'react';
 import { DropdownItemProps, DropdownOption } from './types';
 
-const DropdownItemSelected: FC<DropdownItemProps<DropdownOption>> = memo(({ option, isSelected, onToggleSelect }) => {
+const DropdownItemSelected: FC<DropdownItemProps<DropdownOption>> = memo(({ option, onToggleSelect }) => {
     return (
-        <li
-            key={option.value}
-            className={`dropdown__selected_list__item ${isSelected ? 'selected' : ''}`}
-            onClick={() => onToggleSelect(option)}
-            aria-selected={isSelected}
-        >
+        <li key={option.value} className={`dropdown__selected_list__item`} onClick={() => onToggleSelect(option)}>
             {option.label}
         </li>
     );
