488778322bc72001bcd8cc02c694c907eb822b87
sina <info@sinaghadri.com>
2023-12-19 01:07:40 +0330

    feat - add  useOnClickOutside custom hook

---
+++ src/hooks/useOnClickOutside.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+export function useOnClickOutside<T extends HTMLDivElement>(
+    ref: React.RefObject<T>,
+    handler: (event: MouseEvent | TouchEvent) => void,
+) {
+    useEffect(() => {
+        const listener = (event: MouseEvent | TouchEvent) => {
+            if (ref.current && !ref.current.contains(event.target as Node)) {
+                handler(event);
+            }
+        };
+
+        document.addEventListener('mousedown', listener);
+        document.addEventListener('touchstart', listener);
+
+        return () => {
+            document.removeEventListener('mousedown', listener);
+            document.removeEventListener('touchstart', listener);
+        };
+    }, [ref, handler]);
+}
